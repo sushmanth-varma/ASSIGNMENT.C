a3bff9a6194fb23bcb5b0b21b3bae977e6fa7c66
sushmanth-varma <128605935+sushmanth-varma@users.noreply.github.com>
2023-03-23 21:46:26 +0530

    Create 3assignment7.c

---
+++ 3assignment7.c
@@ -0,0 +1,25 @@
+ #include <stdio.h>
+
+int main () {
+
+int a;
+
+while(a==0)
+for( a = 1; a <= 10; a++ )
+{
+printf("%d\n",a * a);
+}
+
+return 0;
+}
+output:
+1
+4
+9
+16
+25
+36
+49
+64
+81
+100
