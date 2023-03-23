e85cb4d0e638575cffd7541c905a4fe98658af14
sushmanth-varma <128605935+sushmanth-varma@users.noreply.github.com>
2023-03-23 20:48:55 +0530

    Create 3assignment2.c

---
+++ 3assignment2.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+    
+    (ch>='a' && ch<='z') || (ch>='A' && ch<='Z') 
+        ? printf("It is ALPHABET")
+        : printf("It is NOT ALPHABET");
+
+    return 0;
+}
+output:
+Enter any character: 3
+It is NOT ALPHABET
