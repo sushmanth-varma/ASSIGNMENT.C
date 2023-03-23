d9fd986d5e7b62ffb5311a8eab97cac6c87f4ef6
sushmanth-varma <128605935+sushmanth-varma@users.noreply.github.com>
2023-03-23 17:09:56 +0530

    Create 2assignment6.c

---
+++ 2assignment6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char string[]="Hello World";
+display(string);
+}
+void display(char *string)
+{
+printf("%s",string);
+
+}
+
+output:
+Hello World
