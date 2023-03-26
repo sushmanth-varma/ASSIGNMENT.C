b7a3ba9d34e4baa813ca2f573454a7f0efff4def
sushmanth-varma <128605935+sushmanth-varma@users.noreply.github.com>
2023-03-26 16:26:13 +0530

    Create 4assignment1.c

---
+++ 4assignment1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	struct simp 
+	{
+		int i = 6;
+		char city[] = "chennai";
+	};
+	struct simp s1;
+	printf("%d",s1.city);
+	printf("%d", s1.i);
+	return 0;
+}
+
+
+
+output:
+
+
+error
